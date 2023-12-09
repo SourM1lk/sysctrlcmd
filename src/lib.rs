feb2a503b4d61f2517c2012cb168bf069161ac2c
SourM1lk <rottenm1lk@proton.me>
2023-12-09 10:04:10 -0600

    removed mac for now

--- src/lib.rs
+++ src/lib.rs
@@ -11,12 +11,12 @@ pub trait SystemCommands {
 mod windows;
 #[cfg(target_os = "linux")]
 mod linux;
-#[cfg(target_os = "macos")]
-mod mac;
+//#[cfg(target_os = "macos")]
+//mod mac;
 
 #[cfg(target_os = "windows")]
 pub use windows::WindowsCommands as SystemCommandsImpl;
 #[cfg(target_os = "linux")]
 pub use linux::LinuxCommands as SystemCommandsImpl;
-#[cfg(target_os = "macos")]
-pub use mac::MacCommands as SystemCommandsImpl;
+//#[cfg(target_os = "macos")]
+//pub use mac::MacCommands as SystemCommandsImpl;
