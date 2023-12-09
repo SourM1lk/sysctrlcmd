83f55157282b9d41d4832f06268a90cc149a4224
SourM1lk <rottenm1lk@proton.me>
2023-12-09 18:52:17 +0300

    example add

---
+++ examples/basic_usage.rs
@@ -0,0 +1,12 @@
+use sysctrlcmd::SystemCommandsImpl;
+
+fn main() {
+    // Attempt to lock the system screen
+    SystemCommandsImpl::lock().expect("Failed to lock the system screen");
+    
+    // SystemCommandsImpl::logoff().expect("Failed to log off");
+    // SystemCommandsImpl::hibernate().expect("Failed to hibernate");
+    // SystemCommandsImpl::suspend().expect("Failed to suspend");
+    // SystemCommandsImpl::restart().expect("Failed to restart");
+    // SystemCommandsImpl::shutdown().expect("Failed to shut down");
+}
